ee0dad56876cdca83583d6fb43142b20ab8493a8
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-12-04 10:18:36 -0600

    added menu functionality

---
+++ app.js
@@ -0,0 +1,20 @@
+const menu = document.getElementById("navigation-menu");
+const closeButton = document.getElementById("close-b");
+const burger = document.getElementById("burger-b");
+const menuItem = document.getElementsByTagName("li");
+
+burger.addEventListener("click", openMenu);
+closeButton.addEventListener("click", closeMenu);
+menuItem[0].addEventListener("click", closeMenu);
+menuItem[1].addEventListener("click", closeMenu);
+menuItem[2].addEventListener("click", closeMenu);
+menuItem[3].addEventListener("click", closeMenu);
+menuItem[4].addEventListener("click", closeMenu);
+
+function openMenu() {
+    menu.style.display = "block";
+}
+
+function closeMenu() {
+    menu.style.display = "none";
+}
